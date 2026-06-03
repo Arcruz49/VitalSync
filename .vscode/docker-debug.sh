f293e4f73cedb85903c7e86cebbb1b3388a8bbba
Arthur Cruz <arthurcruzdbp@gmail.com>
2026-06-03 00:57:56 -0300

    fix gitignore

--- .vscode/docker-debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# The C# extension passes commands like "sh -s" and "/vsdbg/vsdbg --interpreter=vscode"
-# as single arguments with spaces. Using sh -c to execute them correctly inside the container.
-docker exec -i vitalsync-api-1 sh -c "$1"
